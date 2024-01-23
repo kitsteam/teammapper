f2895858064db9bfce22d50dada070ed17cd1079
Jannik Streek <jannik.streek@gmail.com>
2024-01-23 17:30:34 +0100

    save

--- teammapper-backend/src/main.ts
+++ teammapper-backend/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from './app.module';
 import configService from './config.service';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use("/arasaac/api", createProxyMiddleware({
+    target: 'https://api.arasaac.org/api',
+    changeOrigin: true,
+    logger: console
+  }));
+
   await app.listen(configService.getPort());
 }
 bootstrap();
--- teammapper-frontend/src/environments/environment.ts
+++ teammapper-frontend/src/environments/environment.ts
@@ -2,7 +2,9 @@ export const environment = {
   production: false,
   version: '0.4.0',
   name: 'TeamMapper',
-  pictogramApiUrl: 'https://api.arasaac.org/v1/pictograms',
-  pictogramStaticUrl: 'https://static.arasaac.org/pictograms',
+  pictogramApiUrl: '/arasaac/api',
+  pictogramStaticUrl: '/arasaac/images',
+  // pictogramApiUrl: 'https://api.arasaac.org/v1/pictograms',
+  // pictogramStaticUrl: 'https://static.arasaac.org/pictograms',
   featureFlagPictograms: true,
 };
